00dad9559c4b8ff777929a1db7cefadaaf0d3bcd
Nan Baosong <nanbaosong@163.com>
2020-12-15 11:21:46 +0800

    add log for start get data and end

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ filename = 'all_fund_data.xls'
 def get_one_fund_info(url, name, code):
     html = requests.get(url, headers)
     html.encoding='utf-8'
-    soup = BeautifulSoup(html.text, 'lxml')
+    soup = BeautifulSoup(html.text, "html.parser")
     redirect = soup.find(name='head').find(name='script', attrs={'type': 'text/javascript'})
     if redirect != None and redirect.contents[0] != None:
         real_url = redirect.contents[0].split('"')[1]
@@ -22,12 +22,14 @@ def get_one_fund_info(url, name, code):
 def get_all_data(root_url):
     root_html = requests.get(root_url, headers)
     root_html.encoding='utf-8'
-    tmp_str = root_html.text[9:-1]
+    tmp_str = root_html.text.split('=')[-1].split(';')[0]
     all_data = eval(tmp_str)
     all_fund_info = []
     for data in all_data:
         url = 'http://fund.eastmoney.com/' + data[0] + '.html'
+        print('==== start to get data of %s %s ===='%(data[0], data[2]))
         one_fund_info = get_one_fund_info(url, data[2], data[0])
+        print('==== end to get data of %s %s ===='%(data[0], data[2]))
         all_fund_info.append(one_fund_info)
     return all_fund_info
 
