b2de4e39d3ee8e677e75c08b6b3d1d94490eafbb
Nan Baosong <baosong.nan@cerence.com>
2020-12-16 11:37:03 +0800

    add some filter for what you want

--- main.py
+++ main.py
@@ -38,9 +38,28 @@ def get_all_data(root_url):
 
 def get_what_you_want(input, file):
     rank = input.increase_info.rank
-    if input.base_info.fund_size > 30 and rank['oneWeek'] < 0.3 and rank['oneMonth'] < 0.3 and rank['threeMonth'] < 0.3 and rank['sixMonth'] < 0.3 and rank['currentYear'] < 0.3 and rank['oneYear'] < 0.3 and rank['twoYear'] < 0.3:
-        file.writelines(input.base_info.code)
-        file.write('\n')
+    hushen_300 = input.increase_info.hushen_300
+    current_fund = input.increase_info.current_fund
+
+    if input.base_info.fund_size > 20 and\
+       input.base_info.fund_size < 350 and\
+       rank['oneWeek'] < 0.3 and\
+       rank['oneMonth'] < 0.3 and\
+       rank['threeMonth'] < 0.3 and\
+       rank['sixMonth'] < 0.3 and\
+       rank['currentYear'] < 0.3 and\
+       rank['oneYear'] < 0.3 and\
+       rank['twoYear'] < 0.3:
+        if  current_fund['oneWeek'] > hushen_300['oneWeek'] and\
+            current_fund['oneMonth'] > hushen_300['oneMonth'] and\
+            current_fund['threeMonth'] > hushen_300['threeMonth'] and\
+            current_fund['sixMonth'] > hushen_300['sixMonth'] and\
+            current_fund['currentYear'] > hushen_300['currentYear'] and\
+           (current_fund['oneYear'] > hushen_300['oneYear'] or current_fund['oneYear'] == -100000.0) and\
+           (current_fund['twoYear'] > hushen_300['twoYear'] or current_fund['twoYear'] == -100000.0):
+            file.writelines(input.base_info.code)
+            file.write('\n')
+            file.flush()
 
         
 def analyze(all_data):
