b5fb1dd64adc35b3732f965908b5c6fd51280a06
Nan Baosong <baosong.nan@cerence.com>
2020-12-26 23:35:57 +0800

    fix empty bugs

--- analyze_data.py
+++ analyze_data.py
@@ -12,9 +12,14 @@ def analyze_manager(one_fund_info, manager_file):
 
 def analyze_one_fund(one_fund_info, fund_file):
     if is_what_you_want(one_fund_info):
-        fund_file.writelines(('%s  %s  %s  %s  %s  %s  %s  %s  %s\n') % (one_fund_info.base_info.code, one_fund_info.base_info.name, one_fund_info.base_info.fund_type,\
+        fund_file.writelines(('%s  %s  %s  %s  %s  %s  %s  %s') % (one_fund_info.base_info.code, one_fund_info.base_info.name, one_fund_info.base_info.fund_type,\
                         one_fund_info.base_info.create_date, one_fund_info.base_info.current_manager, one_fund_info.base_info.organization,\
-                        one_fund_info.base_info.fund_size, one_fund_info.base_info.fund_rating, one_fund_info.holding_info.internal))
+                        one_fund_info.base_info.fund_size, one_fund_info.base_info.fund_rating))
+        if one_fund_info.holding_info:
+            fund_file.writelines('  %s\n' % (one_fund_info.holding_info.internal))
+        else:
+            fund_file.writelines('  0.0\n')
+
         fund_file.flush()
 
 
@@ -48,7 +53,7 @@ def get_good_manager(input):
             return None
         all_manager_info.append(manager_name_list[index])
         for info in value:
-            if manager_career_length[index] < 5 or\
+            if manager_career_length[index] < 4 or\
                info.increase_amount and info.same_type_ave and info.increase_amount < info.same_type_ave or\
                info.rank_rate and info.rank_rate > 0.25:
                return None
